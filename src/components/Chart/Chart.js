9b4796bedb0ba1aa6f63a140fc48965cf51e4e60
Jess <jesskaria1@gmail.com>
2021-05-08 18:52:12 +0100

    graph fix

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,45 +2,41 @@ import React, { useEffect, useState } from 'react'
 import { Line, Bar } from 'react-chartjs-2'
 import axios from 'axios'
 import moment from 'moment'
+import TotalOrdersData from '../Data/TotalOrdersData.json'
 
 const Chart = () => {
   const [chartData, setChartData] = useState({})
 
   const chart = () => {
-
     const emptyCount = []
     const emptyCreated = []
+    const dateConverter = a => moment(a)
+    const ordersClean = TotalOrdersData.map(item => Object.assign(item, { created_at: dateConverter(item.created_at) }))
 
-    axios.get('../Data/TotalOrdersData.json')
-      .then(resp => {
-        const dateConverter = a => moment(a)
-        const ordersClean = resp.data.map(item => Object.assign(item, { created_at: dateConverter(item.created_at) }))
-
-        const sotedDate = ordersClean.sort(function(a,b) {
-          return a.created_at - b.created_at
-        })
-        for (const dataObj of sotedDate) {
-          emptyCount.push(dataObj.line_items[0].total_price)
-          emptyCreated.push(dataObj.created_at)
+    const sotedDate = ordersClean.sort(function (a, b) {
+      return a.created_at - b.created_at
+    })
+    for (const dataObj of sotedDate) {
+      emptyCount.push(dataObj.line_items[0].total_price)
+      emptyCreated.push(dataObj.created_at)
+    }
+    setChartData({
+      labels: emptyCreated,
+      datasets: [
+        {
+          label: 'Orders over time',
+          data: emptyCount,
+          backgroundColor: [
+            '#fe5000'
+          ],
+          borderWidth: 1
         }
-        setChartData({
-          labels: emptyCreated,
-          datasets: [
-            {
-              label: 'Orders over time',
-              data: emptyCount,
-              backgroundColor: [
-                '#fe5000'
-              ],
-              borderWidth: 1
-            }
-          ]
-        })
-      })
-      .catch(err => {
-        console.log(err)
-      })
+      ]
+    })
+
   }
+
+
   useEffect(() => {
     chart()
   }, [])
@@ -60,7 +56,7 @@ const Chart = () => {
         <div className="content">
           <div className="chart-area">
             <Bar
-              data={chartData} 
+              data={chartData}
               height={'55%'}
               options={{
                 responsive: true,
